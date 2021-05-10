13c15c94be95a5d4a7d2f273bf82b2148c928321
Lucio Vitorio <luciovitorio@yahoo.com.br>
2021-05-10 19:06:39 -0300

    Acertando configuracao do CORS

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
+app.options('*', cors());
 
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
